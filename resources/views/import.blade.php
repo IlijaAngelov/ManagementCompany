e505cf0142a2de727522a92cd7b574e349d6edb2
IlijaAngelov <ilija.angelov@yahoo.com>
2025-03-12 17:44:15 +0100

    refacor:Removed Breeze

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -1,29 +1,24 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Import') }}
-        </h2>
-        <div class="container mt-4">
-            <div>
-                <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="messages">
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
+<h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+    {{ __('Import') }}
+</h2>
+<div class="container mt-4">
+    <div>
+        <form action="{{ route('import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="messages">
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
                     </div>
-                    <div class="fields">
-                        <div class="input-group mb-3">
-                            <input type="file" class="form-control" id="import_csv" name="import_csv" accept=".csv">
-                            <label class="input-group-text" for="import_csv">Upload</label>
-                        </div>
-                    </div>
-                    <button type="submit" class="btn btn-success">Import CSV</button>
-                </form>
+                @endif
             </div>
-        </div>
-    </x-slot>
-</x-app-layout>
-
+            <div class="fields">
+                <div class="input-group mb-3">
+                    <input type="file" class="form-control" id="import_csv" name="import_csv" accept=".csv">
+                    <label class="input-group-text" for="import_csv">Upload</label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-success">Import CSV</button>
+        </form>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,3 @@ Route::get('/import', function () {
 
 Route::post('/import', [ShiftController::class, 'importShifts'])->name('import');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
